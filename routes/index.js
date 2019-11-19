5135ea69e0ba7600089f61fceb67e65b921e8e7a
Pavlitsa <pzafeiri@gmail.com>
2019-11-19 16:34:39 +0100

    merging to dev

--- routes/index.js
+++ routes/index.js
@@ -94,32 +94,5 @@ router.post("/cafes", loginCheck(), (req, res, next) => {
 //   res.render("cafeDetails.hbs");
 // });
 
-<<<<<<< HEAD
-=======
-router.get("/api/points", (req, res, next) => {
-  Point.find()
-    .then(points => {
-      res.json(points);
-    })
-    .catch(err => {
-      next(err);
-    });
-});
-
-router.post("/api/points", (req, res, next) => {
-  // retrieve coordinates from req.body
-  console.log(req.body);
-  // use these coordinates to create a Point
-  Point.create({
-    coordinates: req.body.coordinates
-  })
-    .then(() => {
-      res.json();
-    })
-    .catch(err => {
-      next(err);
-    });
-});
->>>>>>> auth
 
 module.exports = router;
