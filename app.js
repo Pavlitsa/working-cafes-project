3afc24c56e94ef6ea11dca966f989c0db386c876
Pavlitsa <pzafeiri@gmail.com>
2019-11-21 19:51:11 +0100

    google signin fix

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:3000/auth/google/callback"
+      callbackURL: "https://coffeefi.herokuapp.com/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
       User.findOne({ googleId: profile.id })
